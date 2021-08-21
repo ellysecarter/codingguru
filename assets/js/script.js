2fa949fd370b6c9270a2e09820576a1b688044eb
Ellyse <85855848+ellysecarter@users.noreply.github.com>
2021-08-21 01:09:39 -0700

    updated js file

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+
+var generateBtn = document.querySelector("#code");
+
+
+// write questions here
+function startQuiz() {
+ var codeWinner = 
+
+      var ques1 = "What is the process of finding errors and fixing them within a program"
+      var ques2 = "A loop that never ends is referred to as a what?"
+      var ques3= "Which command will stop an infinite loop?";
+      var ques4 = "During program development, software requirements specify";
+      var ques5 = "Which of the sets of statements below will add 1 to x if x is positive and subtract 1 from x if x is negative but leave x alone if x is 0?";
+
+    if var
+
+
+
+}function startQuiz()
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", startQuiz);
+
+
+
+// ans1c
+var ans1a = "Compiling"
+var ans1b = "Executing"
+var ans1c = "Debugging"
+var ans1d = "Scanning"
+ 
+// ans2b
+var ans2a = "While loop"
+var ans2b = "Infinite loop"
+var ans2c = "Recursive loop"
+var ans2d = "for loop"
+
+// ans3d
+var ans3a = "Alt - C"
+var ans3b = "Shift - C"
+var ans3c = "Esc"
+var ans3d = "Ctrl - C"
+
+// ans4b
+var ans4a = "How the program will accomplish the task"
+var ans4b = "What the task is that the program must perform"
+var ans4c = "How to divide the task into subtasks"
+var ans4d = "How to test the program when it is done"
+
+// ans5b
+var ans5a = "If (x > 0) x++; else x--;"
+var ans5b = "If (x > 0) x++; else if (x < 0) x--;"
+var ans5c = "If (x == 0) x = 0;else x++;x--;"
+var ans5d = "X++; x--;"
